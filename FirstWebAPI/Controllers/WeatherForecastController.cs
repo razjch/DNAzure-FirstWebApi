2e0d7aa45c206a85bc1dee06419d923a899a7247
RahulRazj <72122325+RahulRazj@users.noreply.github.com>
2021-10-01 22:54:21 +0545

    Added a string

--- FirstWebAPI/Controllers/WeatherForecastController.cs
+++ FirstWebAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace FirstWebAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "hello"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
